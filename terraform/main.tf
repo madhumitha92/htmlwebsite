4266be97366bd33c74d5c0fb3d02f3f9fe14fa06
mad <mad@gmail.com>
2024-03-24 18:41:35 -0700

    added security group for ec2

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,10 +11,20 @@ provider "aws" {
   # Define AWS provider with the desired region
 }
 
+resource "aws_security_group" "sec_group" {
+  name   = "sec_group"
+  vpc_id = "vpc-3324nnrvdl"
+}
+
+data "aws_security_group" "testsg" {
+  id = "sg-0903498cfe6b000fb"
+}
+
 resource "aws_instance" "web_server" {
   ami           = "ami-05c969369880fa2c2"
   instance_type = "t2.micro"
   key_name = "linuxkey"
+  vpc_security_group_ids = [data.aws_security_group.testsg.id]
   tags = {
     Name = "WebServer"
   }
